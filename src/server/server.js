1d7f2696aaf5c3d3575c0d6d61fc951005afa7d1
Yuta Katayama <yutapilafu@gmail.com>
2021-05-13 23:47:56 +0900

    i can get data of PixabayAPI

--- src/server/server.js
+++ src/server/server.js
@@ -33,11 +33,13 @@ app.get('/fetchData', async (req, res) => {
         const geoNames = await axios.get(`http://api.geonames.org/searchJSON?country=${req.query.country}&name_equals=${req.query.location}&maxRows=1&username=${process.env.GEO_NAME_USERNAME}`)
         const currentWeatherBit = await axios.get(`https://api.weatherbit.io/v2.0/current?key=${process.env.WAETHERBIT_API_KEY}&lat=${geoNames.data.geonames[0].lat}&lon=${geoNames.data.geonames[0].lng}`)
         const forecastWeatherBit = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WAETHERBIT_API_KEY}&lat=${geoNames.data.geonames[0].lat}&lon=${geoNames.data.geonames[0].lng}`)
-        res.send(forecastWeatherBit.data)
+        const pixabay = await axios.get(`https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${req.query.location}&category=travel&&image_type=photo&orientation=horizontal`)
+        res.send(pixabay.data)
     } catch (error) {
         const { status, statusText, data } = error.response;
         console.log(`Error! HTTP Status: ${status} ${statusText}`);
         console.log('Return error message is below :');
         console.log(data);
+        res.send(data)
     }
 })
