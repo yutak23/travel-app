dd804ad3d6a229b3393af183caa051d270462f44
Yuta Katayama <yutapilafu@gmail.com>
2021-05-13 23:24:42 +0900

    i get weather data by lat/lon

--- src/server/server.js
+++ src/server/server.js
@@ -29,12 +29,15 @@ app.listen(8081, function () {
 })
 
 app.get('/fetchData', async (req, res) => {
-    console.log(req.query)
     try {
-        const data = await axios.get(`http://api.geonames.org/searchJSON?country=${req.query.country}&name_equals=${req.query.location}&maxRows=1&username=${process.env.GEO_NAME_USERNAME}`)
-        console.log(data)
+        const geoNames = await axios.get(`http://api.geonames.org/searchJSON?country=${req.query.country}&name_equals=${req.query.location}&maxRows=1&username=${process.env.GEO_NAME_USERNAME}`)
+        const currentWeatherBit = await axios.get(`https://api.weatherbit.io/v2.0/current?key=${process.env.WAETHERBIT_API_KEY}&lat=${geoNames.data.geonames[0].lat}&lon=${geoNames.data.geonames[0].lng}`)
+        const forecastWeatherBit = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WAETHERBIT_API_KEY}&lat=${geoNames.data.geonames[0].lat}&lon=${geoNames.data.geonames[0].lng}`)
+        res.send(forecastWeatherBit.data)
     } catch (error) {
         const { status, statusText, data } = error.response;
-        console.log(`Error! HTTP Status: ${status} ${statusText}, errMsg : ${data.status.message}`);
+        console.log(`Error! HTTP Status: ${status} ${statusText}`);
+        console.log('Return error message is below :');
+        console.log(data);
     }
 })
