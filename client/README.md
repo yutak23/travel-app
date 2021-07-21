53184d0063a58ab35a275761108c10079a43d937
Yuta Katayama <yutapilafu@gmail.com>
2021-07-21 11:13:12 +0900

    入力時のバリデーション完成

--- client/README.md
+++ client/README.md
@@ -1,24 +1,35 @@
 # client
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 実装時に参考にしたもの
+
+### Validation の実装
+
+- [Vue.js で computed に引数を渡す方法](https://qiita.com/wataru65818460/items/f38898236512f654df4c)<br>vee-validate の slot props の値を使った computed を実装する際に参考になった
--- client/src/views/content/CreateTravel.vue
+++ client/src/views/content/CreateTravel.vue
@@ -2,45 +2,65 @@
   <div class="p-3 bg-light">
     <form class="row g-3 needs-validation" autocomplete="off" novalidate>
       <label class="fs-3">新しい旅行先を作成</label>
-      <div class="col-md-6 form-floating">
-        <input
-          name="country"
-          type="text"
-          class="form-control"
-          placeholder="行先（国名）"
-          v-model="country"
-        />
-        <label for="floatingInput">行先（国名）</label>
+      <div class="col-md-6">
+        <validation-provider name="行先（国名）" rules="required" v-slot="v">
+          <div class="form-floating">
+            <input
+              name="country"
+              type="text"
+              :class="['form-control', validateContry(v)]"
+              placeholder="行先（国名）"
+              v-model="country"
+            />
+            <label for="floatingInput">行先（国名）</label>
+          </div>
+          <div class="mt-1 text-danger">{{ v.errors[0] }}</div>
+        </validation-provider>
       </div>
-      <div class="col-md-6 form-floating">
-        <input
-          name="city"
-          type="text"
-          class="form-control"
-          placeholder="行先（都市名）"
-          v-model="city"
-        />
-        <label for="floatingInput">行先（都市名）</label>
+      <div class="col-md-6">
+        <validation-provider name="行先（都市名）" rules="required" v-slot="v">
+          <div class="form-floating">
+            <input
+              name="city"
+              type="text"
+              :class="['form-control', validateCity(v)]"
+              placeholder="行先（都市名）"
+              v-model="city"
+            />
+            <label for="floatingInput">行先（都市名）</label>
+          </div>
+          <div class="mt-1 text-danger">{{ v.errors[0] }}</div>
+        </validation-provider>
       </div>
-      <div class="col-md-6 form-floating">
-        <input
-          name="departureDate"
-          type="date"
-          class="form-control"
-          placeholder="出発日"
-          v-model="departureDate"
-        />
-        <label for="floatingInput">出発日</label>
+      <div class="col-md-6">
+        <validation-provider name="出発日" rules="required" v-slot="v">
+          <div class="form-floating">
+            <input
+              name="departureDate"
+              type="date"
+              :class="['form-control', validateDepartureDate(v)]"
+              placeholder="出発日"
+              v-model="departureDate"
+            />
+            <label for="floatingInput">出発日</label>
+          </div>
+          <div class="mt-1 text-danger">{{ v.errors[0] }}</div>
+        </validation-provider>
       </div>
-      <div class="col-md-6 form-floating">
-        <input
-          name="returnDate"
-          type="date"
-          class="form-control"
-          placeholder="帰還日"
-          v-model="returnDate"
-        />
-        <label for="floatingInput">帰還日</label>
+      <div class="col-md-6">
+        <validation-provider name="帰還日" rules="required" v-slot="v">
+          <div class="form-floating">
+            <input
+              name="returnDate"
+              type="date"
+              :class="['form-control', validateReturnDate(v)]"
+              placeholder="帰還日"
+              v-model="returnDate"
+            />
+            <label for="floatingInput">帰還日</label>
+          </div>
+          <div class="mt-1 text-danger">{{ v.errors[0] }}</div>
+        </validation-provider>
       </div>
       <div class="form-floating">
         <button
@@ -70,6 +90,13 @@
 </template>
 
 <script>
+import { ValidationProvider, localize, extend } from "vee-validate";
+import { required } from "vee-validate/dist/rules";
+import ja from "vee-validate/dist/locale/ja.json";
+
+extend("required", required);
+localize("ja", ja);
+
 export default {
   data() {
     return {
@@ -79,8 +106,36 @@ export default {
       returnDate: ""
     };
   },
+  computed: {
+    validateContry() {
+      return v => validate(v);
+    },
+    validateCity() {
+      return v => validate(v);
+    },
+    validateDepartureDate() {
+      return v => validate(v);
+    },
+    validateReturnDate() {
+      return v => validate(v);
+    }
+  },
+  components: {
+    ValidationProvider
+  },
   methods: {
     create() {}
+  }
+};
+
+/**
+ * validationの共通処理
+ */
+const validate = slotPops => {
+  if (slotPops.touched) {
+    return slotPops.errors[0] ? "is-invalid" : "is-valid";
+  } else {
+    return "";
   }
 };
 </script>
