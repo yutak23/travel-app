8e76a4c9153c138f0d1ba4351ffcb1a4f5527eb1
Yuta Katayama <yutapilafu@gmail.com>
2021-07-27 21:24:07 +0900

    デフォルトで表示されるサイトをrouterで設定

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import HelloWorld from "../components/HelloWorld.vue";
 import CreateTravel from "../views/content/CreateTravel.vue";
 
 Vue.use(Router);
@@ -8,6 +9,9 @@ Vue.use(Router);
 export default new Router({
     mode: "history",
     routes: [{
+        path: "/",
+        component: HelloWorld
+    }, {
         path: "/new",
         component: CreateTravel,
     }]
